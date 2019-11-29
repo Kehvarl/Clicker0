29744835d657ed56a8727e838f3944e30a0022cf
Kehvarl <kehvarl@kehvarl.com>
2019-11-29 11:56:11 -0700

    Start setting up element

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,39 @@
 module Main exposing (..)
 
+import Browser
 import Html exposing (..)
 
 
+
+--MAIN
+
+
+main : Program () Model msg
 main =
-    view ()
+    Browser.element
+        { init = init
+        , view = view
+        , update = \msg model -> ( Model 0, Cmd.none )
+        , subscriptions = \model -> Sub.none
+        }
 
 
-view : () -> Html msg
-view () =
+type alias Model =
+    { clicks : Int }
+
+
+
+--INIT
+
+
+init _ =
+    ( Model 0, Cmd.none )
+
+
+
+--VIEW
+
+
+view : Model -> Html msg
+view _ =
     div [] [ text "Click me" ]
